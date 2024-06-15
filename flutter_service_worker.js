77fcbea30f3d596160fe35cdb939e3dce0b88ef1
sumanthnani10 <sumanthnani10@gmail.com>
2024-06-15 00:49:05 -0400

    SMS

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,6 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
+<<<<<<< HEAD
+const RESOURCES = {"assets/AssetManifest.bin": "7ed08066bf01f729f254fe65a870586e",
+"assets/AssetManifest.bin.json": "62d7b7de188362259bd01f57b52d8bbd",
+=======
 const RESOURCES = {".git/COMMIT_EDITMSG": "74b65abe6cd3af4c22adfd3fe940641c",
 ".git/config": "32f47c4478863ba356cd2c976cc05102",
 ".git/description": "a0a7c3fff21f2aea3cfa1d0316dd816c",
@@ -103,10 +107,41 @@ const RESOURCES = {".git/COMMIT_EDITMSG": "74b65abe6cd3af4c22adfd3fe940641c",
 ".git/refs/heads/master": "7f7e4595a31b9aa2647ee60255ab2404",
 ".git/refs/remotes/origin/master": "7f7e4595a31b9aa2647ee60255ab2404",
 "assets/AssetManifest.bin": "7e9b610e9b37aa82920c2c2e124a3c2a",
+>>>>>>> b440bf075794fb249edb7657fd5be50f72700409
 "assets/AssetManifest.json": "4d9f2044b641e0c32ee3b6939323e669",
 "assets/assets/sample.xlsx": "fab2da56b89d80db2a87289e5babf114",
 "assets/assets/srinivas%2520sign.png": "75578b46975adb9a0c183b96d3ae17bd",
 "assets/FontManifest.json": "a056a4108c6ef6c915d9842e8ba342bb",
+<<<<<<< HEAD
+"assets/fonts/MaterialIcons-Regular.otf": "4d985cdbd1cd0f32f4b93d5c73af8ac0",
+"assets/fonts/Poppins-ExtraBold.ttf": "6b78c7ec468eb0e13c6c5c4c39203986",
+"assets/fonts/Poppins-Regular.ttf": "41e8dead03fb979ecc23b8dfb0fef627",
+"assets/fonts/Poppins-SemiBold.ttf": "342ba3d8ac29ac8c38d7cef8efbf2dc9",
+"assets/NOTICES": "ccfe5c66b0dcc954c89f9abf1e82f96f",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
+"assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
+"canvaskit/canvaskit.js": "738255d00768497e86aa4ca510cce1e1",
+"canvaskit/canvaskit.js.symbols": "74a84c23f5ada42fe063514c587968c6",
+"canvaskit/canvaskit.wasm": "9251bb81ae8464c4df3b072f84aa969b",
+"canvaskit/chromium/canvaskit.js": "901bb9e28fac643b7da75ecfd3339f3f",
+"canvaskit/chromium/canvaskit.js.symbols": "ee7e331f7f5bbf5ec937737542112372",
+"canvaskit/chromium/canvaskit.wasm": "399e2344480862e2dfa26f12fa5891d7",
+"canvaskit/skwasm.js": "5d4f9263ec93efeb022bb14a3881d240",
+"canvaskit/skwasm.js.symbols": "c3c05bd50bdf59da8626bbe446ce65a3",
+"canvaskit/skwasm.wasm": "4051bfc27ba29bf420d17aa0c3a98bce",
+"canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
+"flutter_bootstrap.js": "e1f3eb1d7a3df4e0015692ee2026f515",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"index.html": "fca53e3b51cc210aa6bf8f6a89e511f2",
+"/": "fca53e3b51cc210aa6bf8f6a89e511f2",
+"main.dart.js": "20ba57d79793712d1841dc7a5955f9a5",
+"manifest.json": "e1cbb2e91f4afaf4b633f52b810fac4b",
+=======
 "assets/fonts/MaterialIcons-Regular.otf": "ea1115f56b143a0d536d867fecf973e3",
 "assets/fonts/Poppins-ExtraBold.ttf": "6b78c7ec468eb0e13c6c5c4c39203986",
 "assets/fonts/Poppins-Regular.ttf": "41e8dead03fb979ecc23b8dfb0fef627",
@@ -129,12 +164,18 @@ const RESOURCES = {".git/COMMIT_EDITMSG": "74b65abe6cd3af4c22adfd3fe940641c",
 "/": "cec725327bc55485e1e4200d656d63c5",
 "main.dart.js": "07fd029ec4b5c778cb21ae4160cec794",
 "manifest.json": "938f8d14e49f1eb9881ef62a91e089d1",
+>>>>>>> b440bf075794fb249edb7657fd5be50f72700409
 "version.json": "43034c300b57c75993547bf059c69f60"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
 "index.html",
+<<<<<<< HEAD
+"flutter_bootstrap.js",
+"assets/AssetManifest.bin.json",
+=======
 "assets/AssetManifest.json",
+>>>>>>> b440bf075794fb249edb7657fd5be50f72700409
 "assets/FontManifest.json"];
 
 // During install, the TEMP cache is populated with the application shell files.
